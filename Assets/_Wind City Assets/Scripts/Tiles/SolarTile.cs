8259fa4c51592783b5cd294fb446038092da4bba
Seah Ying Xiang <seahyx@gmail.com>
2023-06-21 17:07:24 +0800

    Fixed wind and solar tile

--- Assets/_Wind City Assets/Scripts/Tiles/SolarTile.cs
+++ Assets/_Wind City Assets/Scripts/Tiles/SolarTile.cs
@@ -2,19 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SolarTile : Tiles, Producer
+public class SolarTile : ProducerTile
 {
-    public GameObject model;
-    public int tileID;
-    public Color tileColor;
-
-    void Start() {
-        // Assign values to the public fields
-       // model = "";
-        tileID = 5;
-        tileColor = Color.yellow;
-
-    }
-
-    // Implementations of the Tile interface properties and methods
+    
 }
--- Assets/_Wind City Assets/Scripts/Tiles/WindTile.cs
+++ Assets/_Wind City Assets/Scripts/Tiles/WindTile.cs
@@ -2,19 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WindTile : Tiles, Producer
+public class WindTile : ProducerTile
 {
-    public GameObject model;
-    public int tileID;
-    public Color tileColor;
 
-    void Start() {
-        // Assign values to the public fields
-       // model = "";
-        tileID = 4;
-        tileColor = Color.white;
-
-    }
-
-    // Implementations of the Tile interface properties and methods
 }
