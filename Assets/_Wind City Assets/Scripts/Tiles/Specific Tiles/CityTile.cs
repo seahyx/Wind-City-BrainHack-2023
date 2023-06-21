9fe583c7a55c04e417933f0372f738f371833146
Seah Ying Xiang <seahyx@gmail.com>
2023-06-21 17:26:18 +0800

    Fixed tiles

--- Assets/_Wind City Assets/Scripts/Tiles/Specific Tiles/CityTile.cs
+++ Assets/_Wind City Assets/Scripts/Tiles/Specific Tiles/CityTile.cs
@@ -2,8 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CityTile : Tiles, Producer
+public class CityTile : ConsumerTile
 {
 
-    // Implementations of the Tile interface properties and methods
 }
--- Assets/_Wind City Assets/Scripts/Tiles/Specific Tiles/ForestTile.cs
+++ Assets/_Wind City Assets/Scripts/Tiles/Specific Tiles/ForestTile.cs
@@ -2,8 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ForestTile : Tiles, Environment
+public class ForestTile : EnvironmentTile
 {
 
-    // Implementations of the Tile interface properties and methods
 }
